6260937206240e386ef868ea22fd48f3fef2e6eb
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-28 21:14:31 -0700

    Create Question & Choice models

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
