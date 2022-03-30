8046e1a411eb9a314a7c73c3915fe7514b6a339f
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-30 00:34:33 -0700

    Create tests for ResultsView

--- polls/tests.py
+++ polls/tests.py
@@ -103,6 +103,32 @@ class QuestionDetailViewTests(TestCase):
         self.assertContains(response, past_question.question_text)
 
 
+class QuestionResultsViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The results view of a question with a pub_date in the
+        future returns a 404 not found.
+        """
+        future_question = create_question(
+            question_text='Future question.', days=5
+        )
+        url = reverse('polls:results', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The results view of a question with a pub_date in the past
+        displays the question's results.
+        """
+        past_question = create_question(
+            question_text='Past question.', days=-5
+        )
+        url = reverse('polls:results', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_old_question(self):
         """
