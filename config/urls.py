197e8edb86a36fc10de03e1a1532b457fc793944
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-05 18:34:44 -0700

    Add homepage url

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from polls.views import HomePageView
+
 urlpatterns = [
     # Django admin
     path('admin/', admin.site.urls),
@@ -9,5 +11,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 
     # Local applications
+    path('', HomePageView.as_view(), name='home'),
     path('polls/', include('polls.urls')),
 ]
