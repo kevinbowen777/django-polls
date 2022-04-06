dd2061df5f0205bd279d72c9333c66fa28e96c8d
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-05 18:34:05 -0700

    Set up HomePageView

--- polls/views.py
+++ polls/views.py
@@ -4,8 +4,12 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.utils import timezone
 from django.views import generic
+from django.views.generic import TemplateView
 
 from .models import Choice, Question
+
+class HomePageView(TemplateView):
+    template_name = 'polls/home.html'
 
 
 class IndexView(generic.ListView):
