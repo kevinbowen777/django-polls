e79e7e4255ebb8848a787609153465fe663b55f3
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-16 09:59:34 -0700

    Rewrite tests using setUp method

--- pages/tests.py
+++ pages/tests.py
@@ -3,22 +3,22 @@ from django.urls import reverse
 
 
 class HomePageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
+
     def test_homepage_status_code(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_url_name(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
 
     def test_homepage_template(self):
-        response = self.client.get("/")
-        self.assertTemplateUsed(response, "pages/home.html")
+        self.assertTemplateUsed(self.response, "pages/home.html")
 
     def test_homepage_contains_correct_html(self):
-        response = self.client.get("/")
-        self.assertContains(response, "Homepage")
+        self.assertContains(self.response, "Homepage")
 
     def test_homepage_does_not_contain_incorrect_html(self):
-        response = self.client.get("/")
-        self.assertNotContains(response, "This text does not belong")
+        self.assertNotContains(self.response, "This text does not belong")
