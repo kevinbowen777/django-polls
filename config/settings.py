70060984f7c3e4913290f5a80f6a2f55525d114e
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-15 20:27:31 -0700

    Improve navbar. Fixes #15

--- config/settings.py
+++ config/settings.py
@@ -8,14 +8,8 @@ env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
