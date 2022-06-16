c6a44a009ab741255694589f1f3b998a6a17667a
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-16 15:49:53 -0700

    Add username & bio field to CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models  # noqa: F401
+from django.db import models
+from django.urls import reverse
 
 
 class CustomUser(AbstractUser):
-    pass
+    # first_name and last_name do not cover global name patterns
+    name = models.CharField("Name of User", blank=True, max_length=255)
+    bio = models.TextField("Bio", blank=True)
+
+    def get_absolute_url(self):
+        return reverse("user_detail", kwargs={"username": self.username})
