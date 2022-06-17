c4fd983b16f7450e22eb9cb1537a4f28514d74a1
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-17 14:15:26 -0700

    Move initial profile test to accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -61,3 +61,34 @@ class SignupPageTests(TestCase):
     def test_signup_view(self):
         view = resolve("/accounts/signup/")
         self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
+
+
+"""
+class ProfilePageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("user_detail")
+        self.response = self.client.get(url)
+
+    def test_profilepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_profilepage_url_name(self):
+        response = self.client.get(reverse("user_detail"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_profilepage_template(self):
+        self.assertTemplateUsed(self.response, "account/user_detail.html")
+
+    def test_profilepage_contains_correct_html(self):
+        self.assertContains(self.response, "Profile Page")
+
+    def test_profilepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "This text does not belong")
+
+    def test_profilepage_url_resolves_profilepageview(self):
+        view = resolve("/profile/")
+        self.assertEqual(
+            view.func.__name__,
+            ProfilePageView.as_view().__name__,
+        )
+"""
--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import resolve, reverse
 
-from .views import AboutPageView, HomePageView, ProfilePageView
+from .views import AboutPageView, HomePageView
 
 
 class HomePageTests(SimpleTestCase):
@@ -59,33 +59,4 @@ class AboutPageTests(SimpleTestCase):
         self.assertEqual(
             view.func.__name__,
             AboutPageView.as_view().__name__,
-        )
-
-
-class ProfilePageTests(SimpleTestCase):
-    def setUp(self):
-        url = reverse("profile")
-        self.response = self.client.get(url)
-
-    def test_profilepage_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
-
-    def test_profilepage_url_name(self):
-        response = self.client.get(reverse("profile"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_profilepage_template(self):
-        self.assertTemplateUsed(self.response, "pages/profile.html")
-
-    def test_profilepage_contains_correct_html(self):
-        self.assertContains(self.response, "Profile Page")
-
-    def test_profilepage_does_not_contain_incorrect_html(self):
-        self.assertNotContains(self.response, "This text does not belong")
-
-    def test_profilepage_url_resolves_profilepageview(self):
-        view = resolve("/profile/")
-        self.assertEqual(
-            view.func.__name__,
-            ProfilePageView.as_view().__name__,
         )
