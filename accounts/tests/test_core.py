c0ca1fae407d1e0d8466b0a6ac59b6f7d6f5892f
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-29 15:46:22 -0700

    Re-write user tests with SetUp method

--- accounts/tests/test_core.py
+++ accounts/tests/test_core.py
@@ -5,43 +5,6 @@ from django.urls import resolve, reverse
 from ..views import SignupPageView
 
 
-class CustomUserTests(TestCase):
-    def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(
-            username="kbowen",
-            email="kbowen@example.com",
-            password="testpass123",
-        )
-        self.assertEqual(user.username, "kbowen")
-        self.assertEqual(user.email, "kbowen@example.com")
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
-
-    def test_create_superuser(self):
-        User = get_user_model()
-        admin_user = User.objects.create_superuser(
-            username="superadmin",
-            email="superadmin@example.com",
-            password="testpass123",
-        )
-        self.assertEqual(admin_user.username, "superadmin")
-        self.assertEqual(admin_user.email, "superadmin@example.com")
-        self.assertTrue(admin_user.is_active)
-        self.assertTrue(admin_user.is_staff)
-        self.assertTrue(admin_user.is_superuser)
-
-    def test_get_absolute_url(self):
-        User = get_user_model()
-        user = User.objects.create_user(
-            username="kevin",
-            email="kevin@example.com",
-            password="T3stP@ss123!",
-        )
-        self.assertEqual(user.get_absolute_url(), "/accounts/kevin/")
-
-
 class SignupPageTests(TestCase):
     username = "newuser"
     email = "newuser@example.com"
