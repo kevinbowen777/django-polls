66e70a4287ade4dbbda285f79d896123f0aeecdc
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-20 11:12:24 -0700

    Add age field. Fixes #63

--- accounts/models.py
+++ accounts/models.py
@@ -7,9 +7,13 @@ from django_countries.fields import CountryField
 class CustomUser(AbstractUser):
     # first_name and last_name do not cover global name patterns
     name = models.CharField("Name of User", blank=True, max_length=255)
+    age = models.PositiveIntegerField(null=True, blank=True)
     bio = models.TextField("Bio", blank=True)
     profile_pic = models.ImageField(upload_to="profile_pics/", blank=True)
     country = CountryField("Country", blank=True)
+
+    def __str__(self):
+        return self.username
 
     def get_absolute_url(self):
         return reverse("user_detail", kwargs={"username": self.username})
