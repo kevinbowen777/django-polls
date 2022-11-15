575b83af34e3576cd2ea66e953c706aaa9c13229
Kevin Bowen <kevin.bowen@gmail.com>
2022-11-14 16:11:08 -0800

    Add initial PollFactory

---
+++ polls/tests/factories.py
@@ -0,0 +1,25 @@
+import datetime
+
+from accounts.tests.factories import UserFactory  # noqa: F401
+
+import factory
+import factory.fuzzy
+
+import pytest
+
+from ..models import Question
+
+
+@pytest.fixture
+def question():
+    return QuestionFactory()
+
+
+class QuestionFactory(factory.django.DjangoModelFactory):
+    question_text = factory.fuzzy.FuzzyText(
+        length=25, prefix="What is the best "
+    )
+    pub_date = factory.fuzzy.FuzzyDate(datetime.date(2022, 11, 14))
+
+    class Meta:
+        model = Question
