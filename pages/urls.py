b5a58e2078fe150706a10530a0ac1131c4a94a49
Kevin Bowen <kevin.bowen@gmail.com>
2023-01-01 22:45:50 -0800

    Add email contact page & tests. Resolves #170

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
-from .views import AboutPageView, HomePageView, ProfilePageView
+from .views import (
+    AboutPageView,
+    ContactView,
+    HomePageView,
+    ProfilePageView,
+    SuccessView,
+)
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
+    path("contact/", ContactView, name="contact"),
+    path("success/", SuccessView, name="success"),
     path("profile/", ProfilePageView.as_view(), name="profile"),
 ]
