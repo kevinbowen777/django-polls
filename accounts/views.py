bed818005abb32788f49f73bd21a757b798e4e1e
Kevin Bowen <kevin.bowen@gmail.com>
2024-02-22 16:36:28 -0800

    Update fuff lint & formatter rules. Resolves #395

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 """Views for kbowen-django-polls user accounts."""
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,5 @@
 """Nox sessions - django_polls."""
+
 import tempfile
 
 import nox
@@ -69,7 +70,7 @@ def lint(session):
         session,
         "ruff",
     )
-    session.run("ruff", "check", *args)
+    session.run("ruff", "check", ".", *args)
 
 
 @nox.session(python=PYTHON_VERSIONS)
