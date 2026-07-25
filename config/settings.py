eab8e07d7f7be11992efb452dcfa9aabbd4f21f2
Kevin Bowen <kevin.bowen@gmail.com>
2026-07-25 12:14:58 -0700

    Upgrade Django to 6.0.7. Resolves #584

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,6 @@ TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_L10N = True
 
 USE_TZ = True
 
@@ -137,7 +136,14 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
@@ -145,7 +151,6 @@ MEDIA_ROOT = BASE_DIR / "media"
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 # Default primary key field type
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # django-allauth config
 LOGIN_REDIRECT_URL = "poll_list"
@@ -210,7 +215,7 @@ CRISPY_CLASS_CONVERTERS = {
     "passwordinput": "textinput textInput",
 }
 
-ADMINS = [("Kevin Bowen", "kevinbowen@protonmail.com")]
+ADMINS = ["kevinbowen@protonmail.com"]
 MANAGERS = ADMINS
 
 # LOGGING
--- polls/admin.py
+++ polls/admin.py
@@ -8,6 +8,7 @@ class ChoiceInline(admin.TabularInline):
     extra = 5
 
 
+@admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
@@ -20,6 +21,3 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ("question_text", "pub_date", "was_published_recently")
     list_filter = ["pub_date"]
     search_fields = ["question_text"]
-
-
-admin.site.register(Question, QuestionAdmin)
